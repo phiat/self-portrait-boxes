68bf5af7412bd83aae5d34ddf38407890e5d8324
Patrick Ryan <phiat99@gmail.com>
2013-09-27 19:47:35 -0400

    added video.jar

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject self-portrait-boxes "0.1.0-SNAPSHOT"
   :description "A Clojure library designed to recreate a Processing sketch I made a while back."
-  :url "https://github.com/phiat/self-portrait-boxes"
+  :url "https://github.com/phiat/self-portrait-boxes "
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :java-source-paths ["src/java"]
+  
   :dependencies [[org.clojure/clojure "1.5.1"]
   				 [quil "1.6.0"]])
 
