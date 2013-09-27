5dc83d99ba6e41212611ddfd5cbb36580d3d9bdf
Patrick Ryan <phiat99@gmail.com>
2013-09-27 18:50:48 -0400

    basic p3d sketch working

--- src/self_portrait_boxes/core.clj
+++ src/self_portrait_boxes/core.clj
@@ -7,14 +7,15 @@
   (background 0))                
                                  
 (defn draw []
-
+	(box 20 20 20)
 	)       
 
 (defsketch self-portrait-boxes
   :title "self portrait"  
   :setup setup                      
   :draw draw                        
-  :size [640 480])                  
+  :size [640 480]
+  :renderer :p3d)
 
 
   
