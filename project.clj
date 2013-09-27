901b0bf1e8d6e108faf8db88aa09e92d95ff1da7
Patrick Ryan <phiat99@gmail.com>
2013-09-27 18:29:30 -0400

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject self-portrait-boxes "0.1.0-SNAPSHOT"
   :description "A Clojure library designed to recreate a Processing sketch I made a while back."
-  :url "https://"
+  :url "https://github.com/phiat/self-portrait-boxes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
